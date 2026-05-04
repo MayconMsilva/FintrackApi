8834a3fda3e262445f45379e1352b14a9aa20554
JAVA-DIO <mayconmsilva01@gmail.com>
2026-05-04 18:54:13 -0300

    feat: Criado arquivo dto UsuarioRequest

---
+++ src/main/java/com/project/fintrackApi/dto/UsuarioRequestDTO.java
@@ -0,0 +1,11 @@
+package com.project.fintrackApi.dto;
+
+import lombok.Data;
+
+@Data
+public class UsuarioRequestDTO {
+
+    private String nome;
+    private String email;
+    private String senha;
+}
