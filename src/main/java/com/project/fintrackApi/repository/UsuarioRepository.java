2a818c7bd7060c1efea09a1654734cfef1c36724
JAVA-DIO <mayconmsilva01@gmail.com>
2026-05-04 18:53:41 -0300

    feat: Criado UsuarioRepository

---
+++ src/main/java/com/project/fintrackApi/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.project.fintrackApi.repository;
+
+import com.project.fintrackApi.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+
+    Optional<Usuario> findByEmail(String email);
+
+
+    boolean existsByEmail(String email);
+}
