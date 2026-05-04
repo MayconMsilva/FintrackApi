27439122d60920c4328c1fda59a5d36fdce4a476
JAVA-DIO <mayconmsilva01@gmail.com>
2026-05-04 18:53:08 -0300

    feat: Criado Entidade Usuario

---
+++ src/main/java/com/project/fintrackApi/domain/Usuario.java
@@ -0,0 +1,41 @@
+package com.project.fintrackApi.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+
+    private LocalDateTime dataCriacao;
+
+
+    @PrePersist
+    private void prePersist() {
+        this.dataCriacao = LocalDateTime.now();
+    }
+}
