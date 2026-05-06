42015c0ed7e6de95d23faae862595b0a8c894d3b
JAVA-DIO <mayconmsilva01@gmail.com>
2026-05-06 13:48:45 -0300

    feat: Criado UsuarioController

---
+++ src/main/java/com/project/fintrackApi/controller/UsuarioController.java
@@ -0,0 +1,51 @@
+package com.project.fintrackApi.controller;
+
+
+import com.project.fintrackApi.dto.UsuarioRequestDTO;
+import com.project.fintrackApi.dto.UsuarioResponseDTO;
+import com.project.fintrackApi.service.UsuarioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDTO> cadastrar(@Valid @RequestBody
+                                                            UsuarioRequestDTO dto){
+
+        UsuarioResponseDTO response = usuarioService.cadastrar(dto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<UsuarioResponseDTO> buscarPorId(@PathVariable Long id){
+
+        return ResponseEntity.ok(usuarioService.buscarPorId(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listarTodos(){
+
+        return ResponseEntity.ok(usuarioService.listarTodos());
+    }
+}
