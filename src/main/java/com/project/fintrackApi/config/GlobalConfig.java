608034bde17ccdb1b02b01d383616a09b29f0845
JAVA-DIO <mayconmsilva01@gmail.com>
2026-05-07 17:34:07 -0300

    feat: Criado Arquivo de Configuração Global

---
+++ src/main/java/com/project/fintrackApi/config/GlobalConfig.java
@@ -0,0 +1,21 @@
+package com.project.fintrackApi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class GlobalConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
