b3dad90fa4718a3cc70aeb51e075a06f73d3d840
JAVA-DIO <mayconmsilva01@gmail.com>
2026-06-09 16:28:36 -0300

    feat: Criado Arquivo Exception SaldoInsuficiente

---
+++ src/main/java/com/project/fintrackApi/exception/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package com.project.fintrackApi.exception;
+
+public class SaldoInsuficienteException extends RuntimeException {
+    public SaldoInsuficienteException(String message) {
+
+        super(message);
+    }
+}
